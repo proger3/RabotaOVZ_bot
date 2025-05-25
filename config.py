325db2ea3af34571b14526114ae83ef90830e54b
proger3 <igor-shatrov@yandex.ru>
2025-05-25 17:27:27 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "7861669024:AAFKFY1TR_ZE_kmn-nv9D9onQgSM7k-LS7E"  # Получите у @BotFather
+CHANNEL_ID = "@rabota_ovz"    # Или ID чата (например: -100123456789)
+HH_API_URL = "https://api.hh.ru/vacancies"
