c360dbbf7dcc8ed9ce46bc0032de8ab03fd1dc4b
proger3 <igor-shatrov@yandex.ru>
2025-06-02 19:30:55 +0300

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -1,23 +1,19 @@
 from http.server import BaseHTTPRequestHandler
 import json
 from telebot import TeleBot
+from menu import main_menu  # Импорт вашего меню
 
-bot = TeleBot("ВАШ_ТОКЕН")
+bot = TeleBot("7861669024:AAFKFY1TR_ZE_kmn-nv9D9onQgSM7k-LS7E")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Добро пожаловать!", reply_markup=main_menu())
 
 class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         update = json.loads(post_data.decode('utf-8'))
-        
-        if 'message' in update:
-            chat_id = update['message']['chat']['id']
-            bot.send_message(chat_id, "Бот работает на Vercel!")
-        
+        bot.process_new_updates([update])
         self.send_response(200)
         self.end_headers()
-
-def endpoint(request):
-    handler = Handler(request)
-    handler.handle_request()
-    return handler
