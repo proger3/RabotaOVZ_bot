3b5acddc511f907368f8f6ec50071427fbbbfe2e
proger3 <igor-shatrov@yandex.ru>
2025-06-03 20:41:39 +0300

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -2,9 +2,27 @@ from http.server import BaseHTTPRequestHandler
 import os  # Добавьте эту строку
 from telebot import TeleBot
 import json
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 bot = TeleBot(os.getenv('BOT_TOKEN'))
 
+class Handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        try:
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            logger.info(f"Received data: {post_data.decode()}")  # Логируем входящий запрос
+            
+            update = json.loads(post_data.decode('utf-8'))
+            bot.process_new_updates([update])
+            
+        except Exception as e:
+            logger.error(f"Error: {str(e)}", exc_info=True)  # Логируем полную трассировку ошибки
+        
+        self.send_response(200)
+        self.end_headers()
 # Обработчики команд бота
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
